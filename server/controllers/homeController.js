d557908366b786f0a827ac589c0c99e25046c9d8
kyungbeom Kim <kyungbeom8@naver.com>
2023-05-24 03:48:35 +0900

    home refactoring with function

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,6 +1,66 @@
 const model = require("../models");
 const { Sequelize } = require("sequelize");
 
+function sortedData(lectures) {
+  const dayOrder = {
+    mon: 1,
+    tue: 2,
+    wed: 3,
+    thu: 4,
+    fri: 5,
+    sat: 6,
+    sun: 7,
+  };
+
+  lectures.sort((a, b) => {
+    const dayA = dayOrder[a.lecture_day_of_week];
+    const dayB = dayOrder[b.lecture_day_of_week];
+
+    //요일 정렬
+    if (dayA < dayB) {
+      return -1;
+    } else if (dayA > dayB) {
+      return 1;
+    } else {
+      // 동일한 요일 내에서 period를 비교하여 정렬
+      if (a.lecture_period < b.lecture_period) {
+        return -1;
+      } else if (a.lecture_period > b.lecture_period) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  });
+
+  return lectures;
+}
+
+function mappingWithId(sortedLectures) {
+  let lectureData = {};
+  var id = 0;
+
+  //정렬한 데이터를 기반으로 Json 배열 생성
+  sortedLectures.forEach((lecture) => {
+    let lecture_day_of_week = lecture.lecture_day_of_week;
+    let lectureInfo = {
+      id: id++,
+      home_lec: lecture.Lecture.lecture_name,
+      home_lec_class: lecture.Lecture.lecture_room,
+      home_prof: lecture.Lecture.Professor.name,
+      home_lectime: lecture.lecture_period,
+    };
+
+    if (!lectureData[lecture_day_of_week]) {
+      lectureData[lecture_day_of_week] = [];
+      id = 0;
+    }
+    lectureData[lecture_day_of_week].push(lectureInfo);
+  });
+
+  return lectureData;
+}
+
 exports.homeForm = async (req, res) => {
   try {
     // 로그인한 학번을 세션에서 가져옴
@@ -47,61 +107,8 @@ exports.homeForm = async (req, res) => {
     //     ],
     //   });
     // }
-
-    let lectureData = {};
-    var id = 0;
-
-    const dayOrder = {
-      mon: 1,
-      tue: 2,
-      wed: 3,
-      thu: 4,
-      fri: 5,
-      sat: 6,
-      sun: 7,
-    };
-
-    let sortedLectures = lectures.sort((a, b) => {
-      const dayA = dayOrder[a.lecture_day_of_week];
-      const dayB = dayOrder[b.lecture_day_of_week];
-
-      //요일 정렬
-      if (dayA < dayB) {
-        return -1;
-      } else if (dayA > dayB) {
-        return 1;
-      } else {
-        // 동일한 요일 내에서 period를 비교하여 정렬
-        if (a.lecture_period < b.lecture_period) {
-          return -1;
-        } else if (a.lecture_period > b.lecture_period) {
-          return 1;
-        } else {
-          return 0;
-        }
-      }
-    });
-
-    //정렬한 데이터를 기반으로 Json 배열 생성
-    sortedLectures.forEach((lecture) => {
-      let lecture_day_of_week = lecture.lecture_day_of_week;
-      let lectureInfo = {
-        id: id++,
-        home_lec: lecture.Lecture.lecture_name,
-        home_lec_class: lecture.Lecture.lecture_room,
-        home_prof: lecture.Lecture.Professor.name,
-        home_lectime: lecture.lecture_period,
-      };
-
-      if (!lectureData[lecture_day_of_week]) {
-        lectureData[lecture_day_of_week] = [];
-        id = 0;
-      }
-      lectureData[lecture_day_of_week].push(lectureInfo);
-    });
-
-    // 클라이언트에게 JSON 파일 Response
-    res.status(200).json(lectureData);
+    let sortedLectures = sortedData(lectures);
+    res.status(200).json(mappingWithId(sortedLectures));
   } catch (error) {
     console.log("hello here is home 500");
     console.error(error);
