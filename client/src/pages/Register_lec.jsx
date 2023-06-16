87088aa2c5c46f42dfb488e074e56f2d69b768d7
QWEST <icecoffee2500@gmail.com>
2023-06-16 20:32:56 +0900

    ..

--- client/src/pages/Register_lec.jsx
+++ client/src/pages/Register_lec.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./../scss/Register_lec.module.scss";
+import styles from "./../scss/Register_lec.module.scss";
 import { Col, Row } from "react-bootstrap";
 import HeaderNav from "./../component/HeaderNav";
 import Footer from "./../component/Footer";
@@ -15,7 +15,7 @@ export default function Register_lec() {
     return (
         <>
             <HeaderNav />
-            <div className={style.Wrapper}>
+            <div className={styles.Wrapper}>
                 <img src={registerlec} style={{marginTop:"1%", width:"10%", height:"10%"}}/>
                 <Row>
                     <Col sm={2}>
@@ -25,7 +25,7 @@ export default function Register_lec() {
                         <Response_lec />
                     </Col>
                     <Col sm={1}>
-                        <img className={style.img_display} src={arrowshape}/>
+                        <img className={styles.img_display} src={arrowshape}/>
                     </Col>
                     <Col sm={2}>
                         <Request_lec />
